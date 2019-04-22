d38b06401063308a75281037008a8b60a6655088
paul-chrlt <paul.charlet@gmail.com>
2019-04-23 00:02:01 +0200

    getting number of events

--- getCultureEvents.R
+++ getCultureEvents.R
@@ -7,18 +7,29 @@ getCultureEvents <- function(url,cities){
     coordEvents <- data.table(paste(rawEvents$adresseVoieTypeVoieLieu, rawEvents$adresseVoieNomVoieLieu, rawEvents$adresseCodePostalLieu, rawEvents$adresseCommuneLieu))
     colnames(coordEvents) <- "adresses"
     
-    library(tmap)
-    geocode_OSM(coordEvents$adresses[1:6])
+#    library(tmaptools)
+    if(!file.exists("coordinates.csv")){
+        library(prettymapr)
+        coordinates <- geocode(coordEvents$adresses,source="dsk", cache="geocache")
+        coordinates <- coordinates[,c(5,6)]
+        write.csv(coordEvents,"coordinates.csv")
+    }
+    else{coordEvents <- read.csv("coordinates.csv")}
     
     for (city in 1:length(cities$cityLabel)){
-        coordEvents <- cbind(coordEvents,cities$cityLabel[city])
-        
-        
-        straightDistance()
+        cityname <- cities$cityLabel[city]
+        coordinates$distance <- straightDistance(coordinates$lon,coordinates$lat,cities$lon[city],cities$lat[city])
+        names(coordinates)[length(names(coordinates))] <- cityname
     }
-
+    coordinates <- coordinates[!is.na(coordinates$lon),]
+    eventsNumber <- 1:length(cities$cityLabel)
+    names(eventsNumber) <- cities$cityLabel
+    for(city in 3:length(cities[1,])){
+        eventsNumber[city-2] <- sum(coordinates[,city]<10)
+    }
+    eventsNumber
 }
 
-
+coordinates %>% group
 
 url <- "https://www.data.gouv.fr/s/resources/agenda-de-l-offre-culturelle/community/20150724-153623/Agenda24072015.csv"
