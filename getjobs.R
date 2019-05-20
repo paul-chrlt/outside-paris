a69ce5050eaf29b99e43166a887852bec7186d8a
paul-chrlt <paul.charlet@gmail.com>
2019-05-20 11:04:56 +0200

    adding jobs scraping. Needs testing

--- getjobs.R
+++ getjobs.R
@@ -1,12 +1,19 @@
 library(rvest)
+library(stringr)
 
-baseUrl <- "https://www.indeed.fr/"
-xpathresults <- ""
+baseUrl <- "https://www.indeed.fr/jobs?q=&l="
+xpathresults <- '//*[@id="searchCount"]'
 jobsNumber <- integer()
 
-for (city in 1:length(cities$cityLabel)){
-    searchParameter <- cities$cityLabel[city]
-    targetUrl <- paste0(baseUrl,searchParameter)
-    dom <- read_html(targetUrl)
-    jobsNumber <- append(jobsNumber,html_node(dom,xpath = xpathresults))
+getJobs <- function(cities){
+    for (city in 1:length(cities$cityLabel)){
+        searchParameter <- str_to_lower(cities$cityLabel[city])
+        targetUrl <- paste0(baseUrl,searchParameter)
+        dom <- read_html(targetUrl)
+        rawNumber <- html_nodes(dom,xpath = xpathresults)
+        rawNumber <- str_remove_all(rawNumber," ")
+        rawNumber <- str_extract_all(rawNumber,"[0-9]{1,10}")
+        rawNumber <- paste0(rawNumber[[1]][2],rawNumber[[1]][3])
+        jobsNumber <- append(jobsNumber,as.integer(rawNumber))
+    }
 }
