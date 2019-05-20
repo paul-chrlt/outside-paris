4c60a2b8c87355cd2e9b9a8d7d960d307bebeecf
paul-chrlt <paul.charlet@gmail.com>
2019-05-20 11:25:48 +0200

    basic shiny app, needs testing

--- shinyapp/outside-paris/server.R
+++ shinyapp/outside-paris/server.R
@@ -1,16 +1,20 @@
 library(shiny)
-
+library(leaflet)
+load("cities.Rdata")
 # Define server logic
 shinyServer(function(input, output) {
    
   output$distPlot <- renderPlot({
-    
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2] 
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    francemap <- leaflet() %>%
+      addTiles() %>%
+      addCircleMarkers(lng = cities$lon, lat = cities$lat, radius = cities$score/2, label = cities$cityLabel)
+    francemap
+    # # generate bins based on input$bins from ui.R
+    # x    <- faithful[, 2] 
+    # bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    # 
+    # # draw the histogram with the specified number of bins
+    # hist(x, breaks = bins, col = 'darkgray', border = 'white')
     
   })
   
