cf50460cb0472a5fc80071b4e4be7788a43248b3
paul-chrlt <paul.charlet@gmail.com>
2019-05-20 11:19:37 +0200

    init shinyapp

---
+++ shinyapp/outside-paris/ui.R
@@ -0,0 +1,43 @@
+library(shiny)
+
+# Define UI
+shinyUI(fluidPage(
+    
+    # Application title
+    titlePanel("Outside Paris"),
+    
+    # Sidebar with sliders input 
+    sidebarLayout(
+        sidebarPanel(
+            sliderInput("population",
+                        "Importance of population:",
+                        min = -10,
+                        max = 10,
+                        value = 5
+            ),
+            sliderInput("distance",
+                        "Importance of distance to paris:",
+                        min = -10,
+                        max = 10,
+                        value = 5
+            ),
+            sliderInput("culture",
+                        "Importance of culture:",
+                        min = -10,
+                        max = 10,
+                        value = 5
+            ),
+            sliderInput("jobs",
+                        "Importance of employment:",
+                        min = -10,
+                        max = 10,
+                        value = 5
+            ),
+            # Show a plot of the generated distribution
+            mainPanel(
+                plotOutput("distPlot")
+            )
+        )
+    )
+))
+    
